3457b3274e464cbf2679c751c94b34ea7ba066a3
Paul Niggli <58045354+Rommel25@users.noreply.github.com>
2023-02-03 15:36:21 +0100

    Update firebase-config.js

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBxpdzYjw7DC1cpmR_DIJBO7w4dHhBfQsM",
-    authDomain: "rickmorty-9c593.firebaseapp.com",
-    projectId: "rickmorty-9c593",
-    storageBucket: "rickmorty-9c593.appspot.com",
-    messagingSenderId: "926692998588",
-    appId: "1:926692998588:web:32d57955c2079d504fbfb9",
-    measurementId: "G-NDDJDFD7JG"
+    apiKey: "69.420",
+    authDomain: "69.420",
+    projectId: "69.420",
+    storageBucket: "69.420",
+    messagingSenderId: "69.420",
+    appId: "69.420",
+    measurementId: "69.420"
 };
